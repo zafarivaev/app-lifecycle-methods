2791e0d5e8492dc3eb93fc7ab0727e23dcb5f73d
zafarivaev <z.ivaev@mail.ru>
2021-12-31 16:35:55 +0500

    Initial Commit

---
+++ AppLifecycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifecycle
+//
+//  Created by Зафар Иваев on 31/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
